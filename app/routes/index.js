42b057dc0e97813eccf8b6076fe62e930d139140
Victoria Mielke <victoria.mielke@yahoo.com>
2016-02-09 13:59:22 +0000

    finished app

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,57 +1,22 @@
-'use strict';
+var express  = require('express'),
+	router 	 = express.Router();
 
-var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
+router.get('/', function(req, res) {
+  // get header info and save to var
+  var head = req.headers;
+  // split user agent to get browswer name and language
+  var temp = head['user-agent'].split('(');
+  var browser = temp[0];
+  var temp2 = temp[1].split(')');
+  var os = temp2[0];
+  temp = head['accept-language'].split(',');
+  var language = temp[0];
+  var ip = head['x-forwarded-for'];
+  res.render('index', { language: language,
+                        os: os, 
+                        ip: ip,
+                        browser: browser 
+              });	
+});
 
-module.exports = function (app, passport) {
-
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
-	}
-
-	var clickHandler = new ClickHandler();
-
-	app.route('/')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/index.html');
-		});
-
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
-		});
-
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
-
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
-
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
-
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
-
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
-};
+module.exports = router;
--- server.js
+++ server.js
@@ -1,33 +1,18 @@
 'use strict';
 
-var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
+var express 	= require('express'),
+	bodyParser 	= require('body-parser'),
+	indexRoutes = require('./app/routes/index.js'),
+	app 		= express();
 
-var app = express();
 require('dotenv').load();
-require('./app/config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI);
-
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
-
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
+app.use(indexRoutes);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
+	console.log('Node.js started on port ' + port);
 });
